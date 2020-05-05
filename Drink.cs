aa0b78604f1b5428a729d2a9d0fda33fa7b9b7d3
Arthur Sultanov <as@boolqa.com>
2020-05-06 01:26:09 +0300

    QoL changes

--- Drink.cs
+++ Drink.cs
@@ -1,8 +1,12 @@
-﻿namespace ConsoleAppTrpo
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppTrpo
 {
     public abstract class Drink
     {
-        public Drink(long id, string name, int size, decimal price, int energyValue)
+        protected Drink(long id, string name, int size, decimal price, int energyValue)
         {
             Id = id;
             Name = name;
@@ -11,20 +15,51 @@
             EnergyValue = energyValue;
         }
 
-        public long Id { get; set; }
+        private long Id { get; }
 
-        public string Name { get; }
+        private string Name { get; }
 
-        public int Size { get; }
+        private int Size { get; }
 
-        public decimal Price { get; set; }
+        private decimal Price { get; set; }
 
-        public int EnergyValue { get; }
+        private int EnergyValue { get; }
+
+        public static void ChangePrice(IEnumerable<Drink> drinks)
+        {
+            Console.WriteLine($"{Environment.NewLine}enter id to change price: ");
+            var id = Convert.ToInt32(Console.ReadLine());
+            var selectedDrink = drinks.Single(d => d.Id == id);
+            Console.WriteLine($"{Environment.NewLine}selected drink below:{Environment.NewLine}{selectedDrink}");
+
+            Console.WriteLine($"{Environment.NewLine}enter new price:");
+            var price = Convert.ToDecimal(Console.ReadLine());
+            selectedDrink.Price = price;
+            Console.WriteLine(
+                $"{Environment.NewLine}price changed, changed drink below:{Environment.NewLine}{selectedDrink}");
+        }
+
+        public static IList<Drink> Fill()
+        {
+            var drinks = new List<Drink>();
+            for (var i = 0; i < 10; i++)
+                if (i % 2 == 0)
+                {
+                    var isWithMilk = Convert.ToBoolean(i % 2);
+                    drinks.Add(new Coffee(i, $"coffee name{i}", i, i + 10, i + 15, i + 1.5, isWithMilk));
+                }
+                else
+                {
+                    drinks.Add(new Tea(i, $"tea name {i} ", i, i + 20, i + 10, i + 2.5));
+                }
+
+            return drinks;
+        }
 
         public override string ToString()
         {
             return
-                $"{$"id: {Id}", -6} | " +
+                $"{$"id: {Id}",-6} | " +
                 $"{$"name: {Name}",-20} | " +
                 $"{$"price: {Price} rub",-15} | " +
                 $"{$"size: {Size} ml",-12} | " +
--- Program.cs
+++ Program.cs
@@ -1,45 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ConsoleAppTrpo
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var drinks = Fill();
-            
+            var drinks = Drink.Fill();
+
             foreach (var drink in drinks) Console.WriteLine(drink);
-            
-            Console.WriteLine($"{Environment.NewLine}enter id to change price: ");
-            var id = Convert.ToInt32(Console.ReadLine());
-            var selectedDrink = drinks.Single(d => d.Id == id);
-            Console.WriteLine($"{Environment.NewLine}selected drink below:{Environment.NewLine}{selectedDrink}");
-            
-            Console.WriteLine($"{Environment.NewLine}enter new price:");
-            var price = Convert.ToDecimal(Console.ReadLine());
-            selectedDrink.Price = price;
-            Console.WriteLine($"{Environment.NewLine}price changed, changed drink below:{Environment.NewLine}{selectedDrink}");
 
-            Console.ReadLine();
-        }
-
-        private static IList<Drink> Fill()
-        {
-            var drinks = new List<Drink>();
-            for (var i = 0; i < 10; i++)
-                if (i % 2 == 0)
-                {
-                    var isWithMilk = Convert.ToBoolean(i % 2);
-                    drinks.Add(new Coffee(i, $"coffee name{i}", i, i + 10, i + 15, i + 1.5, isWithMilk));
-                }
-                else
-                {
-                    drinks.Add(new Tea(i, $"tea name {i} ", i, i + 20, i + 10, i + 2.5));
-                }
-
-            return drinks;
+            Drink.ChangePrice(drinks);
         }
     }
 }
